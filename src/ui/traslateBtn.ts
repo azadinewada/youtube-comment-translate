9a942b440acffab7610812a930fdab142b722228
linkwanggo <linkwanggo@gmail.com>
2022-12-09 20:36:17 +0800

    使用了一种全新的循环算法，现在翻译内容将能够显示的更完美

--- src/ui/traslateBtn.ts
+++ src/ui/traslateBtn.ts
@@ -2,26 +2,23 @@ import apis from '../tranlate'
 
 type BtnNames = [string, string]
 
+type SourceItem = [name: string, value: string]
+
 class TranslateBtn {
   private lang: string // 语言类型
   private btn: HTMLDivElement // 翻译按钮实例
   private btnTranslateName: string // 多语言`翻译`名
   private btnOriginalName: string // 多语言`原文`名
-  private customContentElement?: HTMLSpanElement // 自定义包裹翻译内容的span元素
-  private source: {
-    sourceText: string
-    nodeTexts: string[]
-  } // 备份原文本内容
+  private sourceContents: SourceItem[] // 备份原文本内容数组
+  private translatedContents: string[] // 备份翻译后的内容数组
   private translatedText: string // 备份翻译内容
   private isTranslated: boolean //标记是否已经被翻译
   constructor(btnNames: BtnNames, lang: string) {
     this.lang = lang
     this.btnTranslateName = btnNames[0]
     this.btnOriginalName = btnNames[1]
-    this.source = {
-      sourceText: '',
-      nodeTexts: [],
-    }
+    this.sourceContents = []
+    this.translatedContents = []
     this.translatedText = ''
     this.isTranslated = false
     this.btn = this._createBtn()
@@ -61,56 +58,87 @@ class TranslateBtn {
     this.isTranslated = isTranslated
     if (isTranslated) {
       // 填充翻译内容
-
-      // 隐藏所有子元素
-      contentTextElement.childNodes.forEach(node => {
+      // 直接在元素上修改值
+      let i = 0
+      let j = 0
+      while (i < contentTextElement.childNodes.length && j < this.translatedContents.length) {
+        const node = contentTextElement.childNodes[i]
+        const translatedContent = this.translatedContents[j]
         const name = node.nodeName
+        // 条件判断
+        if (name === 'IMG') {
+          i++
+          continue
+        } else if (name === 'SPAN') {
+          if (node.textContent === '\n') {
+            i++
+            continue
+          }
+        }
+        if (translatedContent === '') {
+          j++
+          continue
+        }
+        // 赋值
         switch (name) {
           case '#text': {
-            node.nodeValue = ''
+            node.nodeValue = translatedContent
             break
           }
-          case 'SPAN':
-          case 'IMG': {
-            ;(node as HTMLElement).style.visibility = 'hidden'
-            ;(node as HTMLElement).style.position = 'absolute'
+          case 'SPAN': {
+            ;(node as HTMLElement).innerText = translatedContent
+            break
+          }
+          case 'A': {
+            ;(node as HTMLElement).innerText = translatedContent
             break
           }
         }
-      })
-      // 增加翻译内容元素
-      if (!this.customContentElement) {
-        this.customContentElement = document.createElement('span')
-        this.customContentElement.className = 'style-scope yt-formatted-string'
-        this.customContentElement.textContent = this.translatedText
+        i++
+        j++
       }
-      contentTextElement.appendChild(this.customContentElement)
       // 更改按钮名称
       this._changeBtnName(this.btn, this.btnOriginalName)
     } else {
       // 还原翻译内容
-
-      // 移除翻译内容元素
-      if (this.customContentElement) {
-        contentTextElement.removeChild(this.customContentElement)
-      }
-      // 显示所有子元素
-      contentTextElement.childNodes.forEach(node => {
-        let i = 0
+      let i = 0
+      let j = 0
+      while (i < contentTextElement.childNodes.length && j < this.sourceContents.length) {
+        const node = contentTextElement.childNodes[i]
+        const item = this.sourceContents[j]
         const name = node.nodeName
+        // 条件过滤
+        if (name === 'IMG') {
+          i++
+          continue
+        } else if (name === 'SPAN') {
+          if (node.textContent === '\n') {
+            i++
+            continue
+          }
+        }
+        if (item[0] === 'IMG') {
+          j++
+          continue
+        }
+        // 赋值
         switch (name) {
           case '#text': {
-            node.nodeValue = this.source.nodeTexts[i++]
+            node.nodeValue = item[1]
             break
           }
-          case 'SPAN':
-          case 'IMG': {
-            ;(node as HTMLElement).style.removeProperty('visibility')
-            ;(node as HTMLElement).style.removeProperty('position')
+          case 'SPAN': {
+            ;(node as HTMLElement).innerText = item[1]
+            break
+          }
+          case 'A': {
+            ;(node as HTMLElement).innerText = item[1]
             break
           }
         }
-      })
+        i++
+        j++
+      }
       // 更改按钮名称
       this._changeBtnName(this.btn, this.btnTranslateName)
     }
@@ -121,35 +149,41 @@ class TranslateBtn {
    * 记录#text还原信息
    */
   private _recordSourceText(contentTextElement: Element) {
-    let s: string = ''
     contentTextElement.childNodes.forEach(node => {
       const name = node.nodeName
       switch (name) {
         case '#text': {
           const value = node.nodeValue
           if (value) {
-            s += value
-            this.source.nodeTexts.push(value)
+            this.sourceContents.push([name, value])
           }
           break
         }
         case 'SPAN': {
           const text = node.textContent
-          if (text) {
-            s += text
+          if (text && text != '\n') {
+            // 换行元素不记录
+            // &特殊处理 去除\r元素
+            this.sourceContents.push([name, text.replace('\r', '')])
           }
           break
         }
         case 'IMG': {
           const img = (node as Element).getAttribute('alt')
           if (img) {
-            s += img
+            this.sourceContents.push([name, img])
+          }
+          break
+        }
+        case 'A': {
+          const text = node.textContent
+          if (text) {
+            this.sourceContents.push([name, text])
           }
           break
         }
       }
     })
-    this.source.sourceText = s
   }
 
   /**
@@ -158,14 +192,18 @@ class TranslateBtn {
    */
   private _update(contentTextElement: Element) {
     // 使用备份
-    if (this.translatedText) {
+    if (this.translatedContents.length > 0) {
       this._changeState(true, contentTextElement)
       return
     }
     // 查询翻译结果并渲染
+    const queryText = this.sourceContents
+      .filter(item => item[0] !== 'IMG')
+      .map(item => item[1])
+      .join('\n')
     const googleApi = apis.google
     const query = {
-      text: this.source.sourceText,
+      text: queryText,
       to: googleApi.language(this.lang),
     }
     googleApi.api.translate(query).then(rawResult => {
@@ -175,7 +213,7 @@ class TranslateBtn {
         return
       }
       // 备份翻译内容
-      this.translatedText = result.result.join('\n')
+      this.translatedContents = result.result
       // 更改状态
       this._changeState(true, contentTextElement)
     })
@@ -203,7 +241,7 @@ class TranslateBtn {
         return
       }
       // 记录原文本
-      if (!this.source.sourceText) {
+      if (this.sourceContents.length === 0) {
         this._recordSourceText(contentTextElement)
       }
       // 判断是否已经翻译过 已翻译：还原 | 未翻译：翻译
